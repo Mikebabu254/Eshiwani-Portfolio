147e04d37d3102557255aa631a86bf71dc718ed0
Eshiwani Michael <michaelndolo17142@gmail.com>
2026-08-17 16:58:01 +0300

    contact done

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,9 @@ import Services from "./components/Services";
 import Work from "./components/Work";
 import Skills from "./components/Skills";
 import Creative from "./components/Creative";
+import Contact from "./components/Contact";
 
 function App() {
-  
-
   return (
     <div className="portfolio">
       <NavBar/>
@@ -21,43 +20,8 @@ function App() {
       <Work/>
       <Skills/>
       <Creative/>
-
-
-      {/* CONTACT */}
-      <section id="contact" className="contact-section">
-
-        <div className="container">
-
-          <div className="contact-box">
-
-            <p className="section-label">
-              06 — CONTACT
-            </p>
-
-            <h2>
-              Let's build something
-              <span>great together.</span>
-            </h2>
-
-            <p>
-              Have an idea for a website, application
-              or business system? Let's discuss it.
-            </p>
-
-            <a
-              href="mailto:your-email@example.com"
-              className="button primary"
-            >
-              Start a conversation →
-            </a>
-
-          </div>
-
-        </div>
-
-      </section>
+      <Contact/>
       <Footer />  
-
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,39 @@
+function Contact(){
+      return (
+            <section id="contact" className="contact-section">
+
+        <div className="container">
+
+          <div className="contact-box">
+
+            <p className="section-label">
+              06 — CONTACT
+            </p>
+
+            <h2>
+              Let's build something
+              <span>great together.</span>
+            </h2>
+
+            <p>
+              Have an idea for a website, application
+              or business system? Let's discuss it.
+            </p>
+
+            <a
+              href="mailto:your-email@example.com"
+              className="button primary"
+            >
+              Start a conversation →
+            </a>
+
+          </div>
+
+        </div>
+
+      </section>
+      );
+}
+
+export default Contact;
+
